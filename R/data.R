8fef9b43c873646e9f283bec4bcbd08a88ef34d5
Robin Jauss <jauss@uni-leipzig.de>
2020-12-07 14:55:52 +0100

    Added data (OTU Table)

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' OTU_Table
+#' 
+#' Abundance table of OTUs in Microhabitats
+#' 
+#' @format A data frame with several variables:
+#' 
+#' \describe{
+#' \item{\code{SampleID}}{Unique sample identificatior}
+#' \item{\code{TreeID}}{Unique tree identificatior}
+#' \item{\code{TreeSpecies}}{Species of sampled tree}
+#' \item{\code{Microhabitat}}{Sampled Microhabitat}
+#' \item{\code{Stratum}}{Sampled Stratum}
+#' \item{\code{X...}}{OTU Number and taxonomy}
+#' }
+#' 
+"OTU_Table"
